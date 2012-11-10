23c6ea8e691efc977f0bf272097c46ff83f69f6e
Lonnen <chris.lonnen@gmail.com>
2012-11-10 13:58:48 +0000

    early api draft done
    it is not particularly consistent but it's good enough for v1. enables
    access to branches, heuristic for branch version, actuall releases, etc.
    will refine further as the front end comes together

--- observatory/app.py
+++ observatory/app.py
@@ -2,12 +2,10 @@ import logging
 import logging.config
 import os
 
-from flask import (abort, current_app, Flask, jsonify, render_template,
-                    request, Response)
+from flask import abort, Flask, jsonify, render_template
 
 from github import fetch_tags, fetch_tag
-from socorro import (fetch_environment, fetch_environments,
-                        fetch_most_recent_tag, get_version_tuple)
+from socorro import fetch_most_recent_tag, get_version_tuple
 
 
 app = Flask(__name__)
@@ -25,15 +23,18 @@ logger_name = app.config.get("LOGGER_NAME")
 if logger_name:
     logging.root.name = logger_name
 
+
 @app.route("/")
 def index():
-    #return jsonify(fetch_tags())
-    return jsonify(fetch_environments())
-    #return jsonify(fetch_environment('prod'))
-    #return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/versions/next")
-def next_version():
+
+@app.route("/branches")
+def branches():
+    return jsonify({'branches': ['dev', 'stage']})
+
+@app.route("/branches/stage")
+def stage_version():
     """answers what upcoming version will be the next release
 
     this should be the release candidate on the stage branch
@@ -43,7 +44,9 @@ def next_version():
     n[0] = str(int(n[0]) + 1)
     return jsonify({'version': "".join(n)})
 
-@app.route("/versions/dev")
+
+@app.route("/branches/dev")
+@app.route("/branches/master")
 def dev_version():
     """answers what version number is assigned to the current developer version
 
@@ -53,3 +56,22 @@ def dev_version():
     n = list(get_version_tuple(fetch_most_recent_tag()))
     n[0] = str(int(n[0]) + 2)
     return jsonify({'version': "".join(n)})
+
+
+@app.route("/versions")
+def fetch_versions():
+    """answers all currently released versions.
+
+    this does NOT include upcoming versions on dev and stage
+    """
+    return jsonify({"versions": fetch_tags()})
+
+
+@app.route("/versions/<version>")
+def fetch_version(version):
+    """answers details about a specific version"""
+    ref = "refs/tags/v%s" % version
+    for tag in fetch_tags():
+        if tag['ref'] == ref:
+            return jsonify({'version': fetch_tag(tag['object']['sha'])})
+    return abort(404)
--- observatory/socorro.py
+++ observatory/socorro.py
@@ -10,21 +10,24 @@ def fetch_environments():
     """enumerates known environments"""
     return settings.ENVIRONMENTS
 
+
 def fetch_environment(environment):
-    """fetch and environment's status"""
+    """fetch an environment's status"""
     url = settings.ENVIRONMENTS.get(environment, None)
     if not url:
         print "No environment `%s` in settings" % environment
         return ""
     response = requests.get(url)
     if response.status_code is not 200:
-        print "%s - %s" % (respose.status_code, url)
+        print "%s - %s" % (response.status_code, url)
         return ""
     return json.loads(response.text)
+
 
 def fetch_most_recent_tag():
     """fetches the most recent tag for socorro"""
     get_version_tuple = lambda x: x['url'].split('v')[-1].split('.')
+
     def version_comparator(tagX, tagY):
         x, y = get_version_tuple(tagX), get_version_tuple(tagY)
         q = 0
@@ -39,7 +42,9 @@ def fetch_most_recent_tag():
                 return -1
             q += 1
         return 0
+
     return sorted(fetch_tags(), version_comparator)[0]
+
 
 def get_version_tuple(tag):
     """returns the version tuple of a tag object"""
