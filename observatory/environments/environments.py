48a69ff8e887407f9b0f25588f97bf921f9488cc
Lonnen <chris.lonnen@gmail.com>
2012-11-21 18:28:35 -0800

    move to blueprints
    switched to using blueprints for different observatory components.
    rewrote readme to reflect that this app is now tightly coupled to
    Mozilla's deployment of socorro and not generically useful.

---
+++ observatory/environments/environments.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Blueprint, abort, current_app, jsonify
+import requests
+
+environments = Blueprint('environments', __name__, template_folder="templates")
+
+@environments.route('/<environment>')
+def environ(environment='dev'):
+    try:
+        url = current_app.config['ENVIRONMENTS'][environment]
+        response = requests.get(url)
+        if response.status_code is not 200:
+            abort(424)
+        return jsonify({environment: json.loads(response.text)})
+    except KeyError:
+        abort(404)
+
+@environments.route('/')
+def environs():
+    return jsonify(current_app.config['ENVIRONMENTS'])
